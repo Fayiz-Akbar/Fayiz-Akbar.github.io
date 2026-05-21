bc2c54f02389081240e9a1f719f0f3a68f3efb02
Fayiz-Akbar <fayizakbar26@gmail.com>
2026-05-22 00:21:09 +0700

    penambahan tailwind

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Pastikan base diatur ke '/' untuk repositori utama username.github.io
+  base: './', // Pastikan base diatur ke '/' untuk repositori utama username.github.io
 })
